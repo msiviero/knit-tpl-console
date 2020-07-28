02384f92d87783044d2e190fbe4fe3d45e162241
Marco Siviero <m.siviero83@gmail.com>
2020-07-28 18:02:55 +0200

    Create .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  plugins: [
+    "@typescript-eslint",
+  ],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  rules: {
+    quotes: 2,
+    semi: [
+      2,
+      "always"
+    ],
+  }
+};
